25b1093be7018b6d64aa97467983bcfb34195e41
Ghodkis <saurabh.ghodki91@gmail.com>
2022-02-08 05:45:07 +0000

    adding main.tf for ec2

---
+++ terraform/ec2/main.tf
@@ -0,0 +1,53 @@
+terraform {
+
+  required_providers {
+
+    aws = {
+
+      source  = "hashicorp/aws"
+      version = "~>3.27"
+
+    }
+  }
+
+  required_version = ">=0.14.9"
+
+}
+
+
+provider "aws" {
+
+  profile = "default"
+  region  = "us-west-1"
+
+}
+
+
+resource "aws_instance" "app_server" {
+
+  ami           = "ami-009726b835c24a3aa"
+  instance_type = "t2.micro"
+
+  tags = {
+
+    name = var.instance_name
+
+
+  }
+
+}
+
+resource "aws_instance" "app_server_proxy" {
+
+  ami           = "ami-009726b835c24a3aa"
+  instance_type = "t2.micro"
+
+  tags = {
+
+    name = var.proxy_server_name
+
+
+  }
+
+}
+
